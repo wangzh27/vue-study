a09435cb69097dc9a60083bb8e2158869f6453c4
wangzh27 <30455379+wangzh27@users.noreply.github.com>
2017-12-08 07:16:56 +0800

    Add files via upload

---
+++ vue1/src/components/com2.vue
@@ -0,0 +1,19 @@
+<template>
+	<div id="app3">
+  	<p v-if="seen">显示显示显示</p>
+  </div>
+</template>
+export default {
+	
+var app3=new vue({
+	el:"app3",
+	data:{
+	 seen:true	
+	}
+})
+}
+<script>
+</script>
+
+<style>
+</style>
